24beb2e8fedab7a4cd9b20d352e85aa529aed113
Jeremy McRae <jm33@sanger.ac.uk>
2015-10-13 22:41:08 +0100

    tidied up unit test

--- tests/testthat/test_process_call.R
+++ tests/testthat/test_process_call.R
@@ -12,18 +12,14 @@ test_that("process_cnv_call is correct", {
     
     samples = data.frame("individual_id"=sample_ids,
         "is_proband"=c(rep(FALSE, length(sample_ids) - 1), TRUE))
-    probes = read.table(header=TRUE, text="
-            probe A B C D E F G H I J K
-            p1    3 5 4 4 5 4 4 5 2 3 9
-            p2    3 2 3 3 2 4 3 4 2 5 8
-            p3    2 3 3 2 3 3 3 3 2 4 9
-            p4    4 4 4 2 2 3 2 5 3 2 9")
     
     probes = data.frame(matrix(rnorm(length(sample_ids) * 5), nrow=5))
     names(probes) = sample_ids
     
+    # set the child probe values to distant from the population values
     probes[sample_ids[length(sample_ids)]] = rnorm(5, mean=10, sd=1)
     
+    # define the sample IDs for the trio members
     child_id = sample_ids[length(sample_ids)]
     mom_id = sample_ids[1]
     dad_id = sample_ids[2]
